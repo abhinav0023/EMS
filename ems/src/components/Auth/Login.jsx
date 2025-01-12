b4dc7545b1b50ad85c0a795ef5f02f326d1948f3
Abhinav <abhinavhooda223@gmail.com>
2025-01-12 23:38:13 +0530

    form layout

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
     <>
-    
+      <div className="h-screen w-screen flex items-center justify-center">
+        <div className="border-3 border-yellow-600">
+          <form className="flex flex-col items-center justify-center">
+            <input className="text-black border-2 border-red-600 py-4 px-3 my-2 rounded-full outline-none placeholder:text-black" type="email" placeholder="enter your email" />
+            <input className="text-black border-2 border-red-600 py-4 px-3 rounded-full outline-none placeholder:text-black " type="password" placeholder="enter password" />
+            <button className="text-white my-4 border-2 border-blue-600 py-4 px-3 rounded-full outline-none placeholder:text-black "> Please Login</button>
+          </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
