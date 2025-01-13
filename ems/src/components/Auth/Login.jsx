330690d3ef8a173eeb9c5857a5a678f8e9b9cf04
Abhinav <abhinavhooda223@gmail.com>
2025-01-13 22:22:32 +0530

    form layout

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -4,11 +4,11 @@ function Login() {
   return (
     <>
       <div className="h-screen w-screen flex items-center justify-center">
-        <div className="border-3 border-yellow-600">
-          <form className="flex flex-col items-center justify-center">
-            <input className="text-black border-2 border-red-600 py-4 px-3 my-2 rounded-full outline-none placeholder:text-black" type="email" placeholder="enter your email" />
-            <input className="text-black border-2 border-red-600 py-4 px-3 rounded-full outline-none placeholder:text-black " type="password" placeholder="enter password" />
-            <button className="text-white my-4 border-2 border-blue-600 py-4 px-3 rounded-full outline-none placeholder:text-black "> Please Login</button>
+        <div className="border-3 border-yellow-600 ">
+          <form className="border-2 p-4 flex flex-col rounded-3xl items-center justify-center">
+            <input className="text-white border-2 h-2 bg-grey-600 border-red-600 py-4 px-3 my-2 rounded-full outline-none placeholder:text-black" type="email" placeholder="enter your email" />
+            <input className="text-black border-2 h-2 border-red-600 py-4 px-3 rounded-full outline-none placeholder:text-black " type="password" placeholder="enter password" />
+            <button className="text-white my-4 border-2 h-9 border-blue-600 px-3 rounded-full outline-none placeholder:text-black "> Please Login</button>
           </form>
         </div>
       </div>
