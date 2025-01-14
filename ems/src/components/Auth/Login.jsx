9538a1decda03b533a9aeb9896c5c41bf76311e6
Abhinav <abhinavhooda223@gmail.com>
2025-01-14 23:22:04 +0530

    form layout

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -4,7 +4,7 @@ function Login() {
   return (
     <>
       <div className="h-screen w-screen flex items-center justify-center">
-        <div className="border-3 border-yellow-600 ">
+        <div className="border-3 border-yellow-500 ">
           <form className="border-2 p-4 flex flex-col rounded-3xl items-center justify-center">
             <input className="text-white border-2 h-2 bg-grey-600 border-red-600 py-4 px-3 my-2 rounded-full outline-none placeholder:text-black" type="email" placeholder="enter your email" />
             <input className="text-black border-2 h-2 border-red-600 py-4 px-3 rounded-full outline-none placeholder:text-black " type="password" placeholder="enter password" />
