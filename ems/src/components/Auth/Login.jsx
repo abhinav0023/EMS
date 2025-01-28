8b03a78c2fc64d9bf188c0ffe1d646b6cd20c1dc
Abhinav <abhinavhooda223@gmail.com>
2025-01-28 16:01:16 +0530

    completion of login page

--- ems/src/components/Auth/Login.jsx
+++ ems/src/components/Auth/Login.jsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
-  const submitHandler =(e)=>{
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    // prevent default action of form
     e.preventDefault();
-    console.log("form submission");
-  }
+    console.log("Hello guys, form submitted!");
+    console.log(email);
+    console.log(password);
+// reset after submission
+    setEmail("");
+    setPassword("");
+  };
   return (
     <>
       <div className="h-screen w-screen flex items-center justify-center">
         <div className="border-3 border-yellow-500 ">
-          <form onSubmit={(e)=>{
-            submitHandler(e);
-          }} className="border-2 p-4 flex flex-col rounded-3xl items-center justify-center">
-            <input required className="text-black border-2 h-2 bg-grey-600 border-red-600 py-4 px-3 my-2 rounded-full outline-none placeholder:text-black" type="email" placeholder="enter your email" />
-            <input required className="text-black border-2 h-2 border-red-600 py-4 px-3 rounded-full outline-none placeholder:text-black " type="password" placeholder="enter password" />
-            <button className="text-white my-4 border-2 h-9 border-blue-600 px-3 rounded-full outline-none placeholder:text-black ">Please Login</button>
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+            className="border-2 p-4 flex flex-col rounded-3xl items-center justify-center"
+          >
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="text-black border-2 h-2 bg-grey-600 border-red-600 py-4 px-3 my-2 rounded-full outline-none placeholder:text-black"
+              type="email"
+              placeholder="enter your email"
+            />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="text-black border-2 h-2 border-red-600 py-4 px-3 rounded-full outline-none placeholder:text-black "
+              type="password"
+              placeholder="enter password"
+            />
+            <button className="text-white my-4 border-2 h-9 border-blue-600 px-3 rounded-full outline-none placeholder:text-black ">
+              Please Login
+            </button>
           </form>
         </div>
       </div>
