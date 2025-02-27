a3b80bc7df680bdd51b7b23d18724bc1f7bf1914
Abhinav <abhinavhooda223@gmail.com>
2025-02-27 11:03:06 +0530

    start working on admin dashboard

---
+++ ems/src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Stuff/Header'
+
+function AdminDashboard() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default AdminDashboard
--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -13,6 +13,17 @@ function TaskList() {
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae culpa pariatur iure!
             </p>
         </div>
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-300 rounded-2xl'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-700 text-sm text-white px-3 py-1 rounded'>High</h3>
+            <h4 className='text-black text-sm'>23 Feb 2025</h4>
+        </div>
+            <h2 className='mt-5 text-black text-xl font-semibold'>Make a MERN App </h2>
+            <p className='mt-3 text-sm text-black'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae culpa pariatur iure!
+            </p>
+        </div>
        
     </div>
   )
